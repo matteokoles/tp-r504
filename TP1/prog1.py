cec84f292a380d471ac5babcf7c662aa91edab5f
matteo <matteokole@gmail.com>
2026-09-09 17:48:06 +0200

    ajout fonction puissance avec vérification de type

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,6 +1,9 @@
+import fonctions as f
+
 print("Hello, World!")
 
 while True:
-    nombre = float(input("Entrez un nombre : "))
-    carre = nombre ** 2
-    print("Le carré est :", carre)
+    a = int(input("Entrez le premier nombre : "))
+    b = int(input("Entrez le second nombre : "))
+    res = f.puissance(a, b)
+    print("Résultat :", res)
